4ada99870b155e5eee55009ce3984d8e864f4dca
Tahmim Ahmed Shibli <shibli05+gitlab@gmail.com>
2017-04-03 19:05:38 +0600

    starting dir

---
+++ lib/ps_tweet.ex
@@ -0,0 +1,18 @@
+defmodule PsTweet do
+  @moduledoc """
+  Documentation for PsTweet.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PsTweet.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ps_tweet_test.exs
@@ -0,0 +1,8 @@
+defmodule PsTweetTest do
+  use ExUnit.Case
+  doctest PsTweet
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
